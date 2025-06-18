fa10f1fdb8cc3f10b907caf2ae35359dd74862ed
ut <ut.numagotatu@gmail.com>
2025-06-18 19:16:48 +0900

    Fix index pos

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ export { CLIENT_BUCKET_NAME, EXISTS_CLIENT_DIST } from "./client/aws/s3";
 import "./network/aws/cloudfront";
 export { CLOUD_FRONT_URL } from "./network/aws/cloudfront";
 
-import "./api/aws/lambda";
-export { EXISTS_API_DIST, EXISTS_API_LAMBDA_ZIP, API_LAMBDA_FUNCTION_URL } from "./api/aws/lambda";
-
 import "./edge/aws/viewer-request-lambda"
 export { EXISTS_EDGE_DIST, EXISTS_EDGE_LAMBDA_ZIP } from "./edge/aws/viewer-request-lambda";
+
+import "./api/aws/lambda";
+export { EXISTS_API_DIST, EXISTS_API_LAMBDA_ZIP, API_LAMBDA_FUNCTION_URL } from "./api/aws/lambda";
